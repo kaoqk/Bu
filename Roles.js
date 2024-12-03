72a4c3c1dda217711d8a0c6421e817ef04384b4c
kaoqk <bsbo1474@gmail.com>
2024-12-04 00:19:35 +0300

    Add files via upload

---
+++ Roles.js
@@ -0,0 +1,32 @@
+module.exports = {
+    Roles: [
+        { roleID: "", price: 1000000 },
+        { roleID: "", price: 500000 },
+        { roleID: "", price: 250000 },
+        { roleID: "", price: 500000 },
+        { roleID: "", price: 100000 },
+        { roleID: "", price: 85000 },
+        { roleID: "", price: 70000 },
+        { roleID: "", price: 55000 },
+        { roleID: "", price: 40000 },
+        { roleID: "", price: 40000 },
+        // ... الرتب الأخرى
+    ],
+    WarnsRole: [
+        {
+            Warn25 : "" , 
+            warn50 : '', 
+            warn100 : '', 
+        }
+    ],
+    RolesSellers : [
+      "", 
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",]
+}
---
+++ settings.js
@@ -0,0 +1,123 @@
+module.exports = {
+    Owners: ["", "", ""], //الاونرات 
+    RoleCoOwner: '',
+    BankID: '',
+    Probot: '',
+    red : 'http://localhost:5000',
+    port : 5000 ,
+    prefix : '$',
+    لون_الامبيد : '#15FFAF',
+    RommsShop : ["","","","","","","","","","","","","",""], 
+    Rooms : {
+     RoomPosts : '',
+     RoomAds : '',
+     CeatogryPrivteRooms : '',
+     CeatogryPrivte : "",
+     LogRoles : '', 
+     LogPosts : '',
+     LogAds : '', 
+     LogPrivteRooms : '',
+     LogTranscreipt : '',
+     Warns : '',
+     Orders : '',
+     Sug : '', 
+     Feedback : '',
+     Tax : ''
+    },
+    BroadCast : {
+        Tokens : ["","",""],
+    },
+    CetagryTicket: {
+     support : '', 
+     Tasher : '',
+    },
+    Admins : {
+        DiscordStaff : '', 
+        DiscordLeder : '',
+        Kdaa : '',
+    }, 
+    ServerInfo : {
+    serverID : '',
+    line : `https://cdn.discordapp.com/attachments/1113504839696126044/1242779260486160384/20240521_223046.png?ex=6677f261&is=6676a0e1&hm=b53c2145a0ca80b21e9d4d31b08af662a9be21d2b447d55ba1341dd3a24507d8&`, 
+    ApplyImage : 'https://cdn.discordapp.com/attachments/1113504839696126044/1242833678292484167/20240521_222935.jpg?ex=66777c4f&is=66762acf&hm=d986e768a37ad0b1dea41365358ca17cca47649894272a03641c71d62c0f5c47&', 
+    Orders : 'https://cdn.discordapp.com/attachments/1113504839696126044/1242782719583191162/20240521_222832.jpg?ex=66774cda&is=6675fb5a&hm=47483f32d60417eb30d4080453cce2bd8ff1133f097f0ca5c13908b14fc2380d&', 
+    tashfer : 'https://cdn.discordapp.com/attachments/1113504839696126044/1242811736994746408/20240521_222647.jpg?ex=667767e0&is=66761660&hm=151b3ac5f375ee31fa06d0e76a0eaedd2ac9b20ed4cdbdcb8ea14df7d3cd057b&',
+    },
+
+    Apply : {
+        MasoulKbool : '1235869344257413120',
+        staff : {
+            Room : '', 
+            Role : '',
+            type : true, 
+        }, 
+        Kdaa : {
+            Room : '', 
+            Role : '', 
+            type : true, 
+        }, 
+        Waset : {
+            Room : '', 
+            Role : '', 
+            type : true, 
+        }, 
+
+    }, 
+        
+    Fa7s : {
+        staff : {
+            ticket : 2500 , 
+            warn : 1500 ,
+        }, 
+
+        Kdaa : {
+            ticket : 4000 , 
+        }
+    }, 
+    AutoLine : [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+    ], 
+
+     blacklist : {
+        staff : '', 
+        mazad : '',
+        tickets : '', 
+     }, 
+
+     Close_Open : {
+        log : '', 
+        Rooms : [
+            "",
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+            "", 
+        ]
+     }, 
+     Orders : {
+        montgat : {
+            role : '', 
+            room : '', 
+        }, 
+        tsamem : {
+            role : '', 
+            room : '', 
+        }, 
+        devss : {
+            role : '', 
+            room : '', 
+        }, 
+  
+     }
+
+
+}
